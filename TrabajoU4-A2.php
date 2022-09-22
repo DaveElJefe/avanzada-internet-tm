023d66e5832b92b3369306a1eb171b378381b439
David Buenrostro <56892572+DaveElJefe@users.noreply.github.com>
2022-09-22 10:05:03 -0600

    Actividad Jueves 22-09
    Utilizar bootstrap para hacer una rapida pagina

---
+++ TrabajoU4-A2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+    crossorigin="anonymous">
+    
+</head>
+<body>
+    <div class="container border">
+            <section>
+                <div class="row justify-content-md-center align-items-center">
+                    <div class="col-lg-4 col-md-6 col-sm-12 login">
+                        <h1 class="text-center">Formulario de Login</h1>
+                        <form method="get" class="form" action="TrabajoU4-A2-Products.php">
+                            <div class="form-group">
+                                <label for="">Correo Electronico</label>
+                                <input type="email" class="form-control" id="InputEmail1" placeholder="prueba@ejemplo.com">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Contraseña</label>
+                                <input type="password" class="form-control" id="InputPassword" placeholder="* * * * *">
+                            </div>
+                            <button type="submit" class="btn btn-primary col-12 mb-3">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
+    crossorigin="anonymous"></script>
+</body>
+</html>
