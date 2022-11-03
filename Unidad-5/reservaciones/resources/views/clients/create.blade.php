85f04879fe8a6d66b8305f1450ec2c5dd5bc6f11
daveeljefe <david_e_b_01@hotmail.com>
2022-11-03 09:21:14 -0600

    Crear vista para crear cliente

---
+++ Unidad-5/reservaciones/resources/views/clients/create.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario crear cliente</title>
+</head>
+<body>
+    <form method="post" action="http://127.0.0.1:8000/clients">
+        @csrf
+        <label>Nombre:</label>
+        <input type="text" placeholder="Dave" name="name">
+        <br>
+        <label>Correo:</label>
+        <input type="email" placeholder="example@domain.com" name="email">
+        <br>
+        <label>Numero telefonico:</label>
+        <input type="text" placeholder="612-xxx-xx-xx" name="phone_number">
+        <br>
+        <button type="submit">Crear</button>
+    </form>
+</body>
+</html>
